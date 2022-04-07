720d835100bc12b3f5e4038c40635c41d0765631
Jieun Ko <0930ko@naver.com>
2022-04-07 23:33:45 +0900

    Create 10950.cpp

---
+++ 백준/10950.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+
+    for (int i = 0; i < T; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+  
+}
