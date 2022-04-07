082bf707ebe6f2c249460604eca796f48325bf00
Jieun Ko <0930ko@naver.com>
2022-04-07 23:33:05 +0900

    Create 1008.cpp

---
+++ 백준/1008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//1008¹ø
+	double a, b;
+	cin >> a >> b;
+	cout << fixed;
+	cout.precision(9);
+	cout << a / b << endl;
+	return 0;
+}
