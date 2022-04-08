24244d8705f3f256d94a0a43eb872903537df833
Jieun Ko <0930ko@naver.com>
2022-04-08 22:33:33 +0900

    [11654]

---
+++ 백준/CLASS1/[E,R]_[11654]_아스키코드.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	char a;
+	cin >> a;
+	cout << int(a);
+}
+
+
+//문제 리뷰
+//아스키코드 숫자로 출력하기
+//char형으로 받아서 int형으로 출력하기
+//
+//아스키코드 문자를 숫자로 바꾸고 싶을 때
+//문자에서 '0'을 빼줌
+//cout<<a-'0';
